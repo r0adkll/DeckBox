57f8a78f012d3d3112da58f54314ab445076ee6f
Drew Heavner <veedubusc@gmail.com>
2017-11-21 09:55:31 -0500

    Working on unified search view interface

--- app/src/main/java/com/r0adkll/deckbuilder/arch/ui/features/unifiedsearch/SearchFragment.kt
+++ app/src/main/java/com/r0adkll/deckbuilder/arch/ui/features/unifiedsearch/SearchFragment.kt
@@ -8,14 +8,28 @@ import android.view.ViewGroup
 import com.r0adkll.deckbuilder.R
 import com.r0adkll.deckbuilder.arch.ui.components.BaseFragment
 import com.r0adkll.deckbuilder.arch.ui.features.deckbuilder.di.DeckBuilderComponent
+import com.r0adkll.deckbuilder.arch.ui.features.search.adapter.SearchResultsRecyclerAdapter
 import com.r0adkll.deckbuilder.arch.ui.features.unifiedsearch.di.UnifiedSearchModule
+import kotlinx.android.synthetic.main.fragment_search.*
 
 
 class SearchFragment : BaseFragment() {
 
 
+    private lateinit var adapter: SearchResultsRecyclerAdapter
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        adapter = SearchResultsRecyclerAdapter(activity!!)
+        adapter.setEmptyView(emptyView)
+        adapter.setOnItemLongClickListener { true }
     }
 
 
