20777641087ef0998edf99f4444377150e69d862
Drew Heavner <veedubusc@gmail.com>
2018-08-11 15:12:13 -0400

    Fixed #3 - Improved the robustness of the decklist importer

--- app/src/main/java/com/r0adkll/deckbuilder/arch/data/features/ptcgo/repository/DefaultPTCGOConverter.kt
+++ app/src/main/java/com/r0adkll/deckbuilder/arch/data/features/ptcgo/repository/DefaultPTCGOConverter.kt
@@ -2,17 +2,16 @@ package com.r0adkll.deckbuilder.arch.data.features.ptcgo.repository
 
 import android.annotation.SuppressLint
 import com.r0adkll.deckbuilder.arch.data.AppPreferences
-import com.r0adkll.deckbuilder.arch.domain.features.cards.model.Expansion
+import com.r0adkll.deckbuilder.arch.data.features.ptcgo.model.CardSpec
+import com.r0adkll.deckbuilder.arch.data.features.ptcgo.parser.DeckListParser
 import com.r0adkll.deckbuilder.arch.domain.features.cards.model.PokemonCard
 import com.r0adkll.deckbuilder.arch.domain.features.cards.repository.CardRepository
-import com.r0adkll.deckbuilder.arch.domain.features.decks.model.Deck
 import com.r0adkll.deckbuilder.arch.domain.features.ptcgo.repository.PTCGOConverter
 import io.pokemontcg.model.SuperType
 import io.pokemontcg.model.Type
 import io.pokemontcg.model.Type.*
 import io.reactivex.Observable
 import timber.log.Timber
-import java.io.StringReader
 import javax.inject.Inject
 
 
@@ -21,12 +20,15 @@ class DefaultPTCGOConverter @Inject constructor(
         val preferences: AppPreferences
 ) : PTCGOConverter {
 
+    private val parser = DeckListParser()
+
+
     @SuppressLint("CheckResult")
     override fun import(deckList: String): Observable<List<PokemonCard>> {
         return repository.getExpansions()
                 .onErrorReturnItem(emptyList())
                 .flatMap {
-                    val cards = parsePtcgoDeckList(it, deckList)
+                    val cards = parser.parse(it, deckList)
                     val ids = cards.map { it.id }
                     repository.find(ids)
                             .map { pokes ->
@@ -136,60 +138,13 @@ class DefaultPTCGOConverter @Inject constructor(
     }
 
 
-    private fun parsePtcgoDeckList(expansions: List<Expansion>, deckList: String): List<CardSpec> {
-        val reader = StringReader(deckList)
-        val lines = reader.readLines()
-
-        val cards = ArrayList<CardSpec>()
-        lines.forEach { line ->
-            if (line.startsWith("* ")) {
-                val count = parseCount(line)
-                if (count != null) {
-                    val setInfo = parseSetInformation(line)
-                    val name = parseName(line)
-                    val expansion = expansions.find { it.ptcgoCode == setInfo.first }
-                    val id = "${expansion?.code}-${setInfo.second}"
-                    cards.add(CardSpec(count, name, setInfo.first, setInfo.second, id))
-                }
-            }
-        }
-
-        return cards
-    }
-
-
-    private fun parseExpectedCount(line: String, prefix: String): Int {
-        val remaining = line.replace(prefix, "").replace("-", "").trim()
-        return remaining.toIntOrNull() ?: 0
-    }
-
-
-    private fun parseCount(line: String): Int? {
-        val countPart = line.substring(2, line.indexOf(" ", 2))
-        return countPart.toIntOrNull()
-    }
-
-
-    private fun parseSetInformation(line: String): Pair<String, String> {
-        val parts = line.trim().split(" ")
-        val number = parts.last()
-        val set = parts[parts.size - 2]
-        return Pair(set, number)
-    }
-
-
-    private fun parseName(line: String): String {
-        val clean = line.substring(2).trim()
-        val parts = clean.split(" ").toMutableList()
-        val nameParts = parts.dropLast(2)
-        return nameParts.joinToString(" ")
-    }
-
-
     private fun filterEnergy(): (CardSpec) -> Boolean {
         return { spec ->
             Type.VALUES.filter { it != COLORLESS && it != UNKNOWN && it != DRAGON}
-                    .find { spec.name.contains("${it.name} Energy", true) } != null
+                    .find {
+                        spec.name.contains("${it.name} Energy", true) ||
+                                (spec.name.contains(it.name, true) && spec.set.contains("Energy", true))
+                    } != null
         }
     }
 
@@ -197,20 +152,14 @@ class DefaultPTCGOConverter @Inject constructor(
     private fun mapEnergy(): (CardSpec) -> Pair<CardSpec, String>? {
         return { spec ->
             val type = Type.VALUES.filter { it != COLORLESS && it != UNKNOWN && it != DRAGON }
-                    .find { spec.name.contains("${it.name} Energy", true) }
+                    .find {
+                        spec.name.contains("${it.name} Energy", true) ||
+                                (spec.name.contains(it.name, true) && spec.set.contains("Energy", true))
+                    }
             type?.let {
                 val defaultEnergySet = preferences.basicEnergySet.get()
                 defaultEnergySet.convert(it)?.let { Pair(spec, it) }
             }
         }
     }
-
-
-    data class CardSpec(
-            val count: Int,
-            val name: String,
-            val set: String,
-            val number: String,
-            val id: String
-    )
 }
