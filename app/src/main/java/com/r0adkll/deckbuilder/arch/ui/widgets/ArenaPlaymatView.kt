d4f515aa526a7f3e67d5c1ee10990840ab191e76
Drew Heavner <veedubusc@gmail.com>
2018-09-01 21:57:37 -0400

    Worked out the 'Basic' rendering layout for the playmat view

--- app/src/main/java/com/r0adkll/deckbuilder/arch/ui/widgets/ArenaPlaymatView.kt
+++ app/src/main/java/com/r0adkll/deckbuilder/arch/ui/widgets/ArenaPlaymatView.kt
@@ -2,7 +2,6 @@ package com.r0adkll.deckbuilder.arch.ui.widgets
 
 import android.content.Context
 import android.graphics.*
-import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
 import com.ftinc.kit.kotlin.extensions.color
@@ -20,10 +19,11 @@ class ArenaPlaymatView @JvmOverloads constructor(
     private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val silhouettePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val silhouetteTextPaint: Paint = Paint(Paint.LINEAR_TEXT_FLAG)
+    private val cardPunchPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     private val pikaCoinImage: Bitmap
 
-    private val silhouetteMargin: Float = dpToPx(32f)
+    private val benchMargin: Float = dpToPx(24f)
     private val silhouetteMarginInside: Float = dpToPx(16f)
     private val cardRadius = dpToPx(4f)
     private var silhouetteWidth: Float = 0f
@@ -38,7 +38,7 @@ class ArenaPlaymatView @JvmOverloads constructor(
 
         silhouettePaint.color = color(R.color.playmat)
         silhouettePaint.style = Paint.Style.STROKE
-        silhouettePaint.strokeWidth = dpToPx(6f)
+        silhouettePaint.strokeWidth = dpToPx(4f)
         silhouettePaint.strokeCap = Paint.Cap.ROUND
 
         silhouetteTextPaint.color = color(R.color.playmat)
@@ -46,13 +46,17 @@ class ArenaPlaymatView @JvmOverloads constructor(
         silhouetteTextPaint.typeface = Typeface.DEFAULT_BOLD
         silhouetteTextPaint.textAlign = Paint.Align.CENTER
 
+        cardPunchPaint.color = Color.WHITE
+        cardPunchPaint.style = Paint.Style.FILL_AND_STROKE
+        cardPunchPaint.strokeWidth = dpToPx(4f)
+
         pikaCoinImage = BitmapFactory.decodeResource(resources, R.drawable.ic_pika_coin)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-        silhouetteWidth = (measuredWidth.toFloat() -  ((silhouetteMargin * 2)  +  silhouetteMarginInside * 4)) / 5f
+        silhouetteWidth = (measuredWidth.toFloat() -  ((benchMargin * 2)  +  silhouetteMarginInside * 4)) / 5f
         silhouetteHeight = silhouetteWidth * PokemonCardView.RATIO
     }
 
@@ -64,27 +68,32 @@ class ArenaPlaymatView @JvmOverloads constructor(
         drawPokeBall(canvas)
 
         /*
-         * Draw the Bench
+         * Draw the Benches
          */
-        val baseX = silhouetteMargin
-        val baseY = measuredHeight - (silhouetteMargin + silhouetteHeight)
+        val baseX = benchMargin
+        val baseY = measuredHeight - (benchMargin + silhouetteHeight)
         (0 until 5).forEach {
             val offsetX = (it * silhouetteWidth) + (it * silhouetteMarginInside)
             drawCardSilhouette(canvas, baseX + offsetX, baseY)
         }
 
+        (1 until 6).forEach {
+            val offsetX = measuredWidth - ((it * silhouetteWidth) + ((it - 1) * silhouetteMarginInside))
+            drawCardSilhouette(canvas, offsetX - benchMargin, benchMargin)
+        }
+
         /*
-         * Draw the Deck
+         * Draw the Deck & Discard
          */
         val deckX = baseX + (4 * silhouetteWidth) + (4 * silhouetteMarginInside)
-        val deckY = baseY - (silhouetteHeight + silhouetteMarginInside)
+        val deckY = baseY - ((silhouetteHeight + silhouetteMarginInside) * 2) + (silhouetteMarginInside / 2)
         drawCardSilhouette(canvas, deckX, deckY, "DECK")
+        drawCardSilhouette(canvas, deckX, deckY + (silhouetteHeight + silhouetteMarginInside/2), "DISC.")
 
-        /*
-         * Draw the Discard
-         */
-
-
+        val opponentDeckX = benchMargin
+        val opponentDeckY = benchMargin + silhouetteHeight + silhouetteMarginInside + silhouetteHeight + (silhouetteMarginInside / 2f)
+        drawCardSilhouette(canvas, opponentDeckX, opponentDeckY, "DECK")
+        drawCardSilhouette(canvas, opponentDeckX, opponentDeckY - (silhouetteHeight + silhouetteMarginInside/2f), "DISC.")
     }
 
 
@@ -98,12 +107,27 @@ class ArenaPlaymatView @JvmOverloads constructor(
 
     private fun drawPokeBall(canvas: Canvas) {
         val centerX = measuredWidth / 2f
-        val radius = measuredWidth / 4f
+        val centerY = measuredHeight / 2f
+        val radius = measuredWidth / 5f
         val innerRadius = radius * .40f
 
-        canvas.drawCircle(centerX, 0f, innerRadius, paint)
-        canvas.drawCircle(centerX, 0f, radius, paint)
-        canvas.drawLine(centerX - radius, 0f, centerX - innerRadius, 0f, paint)
-        canvas.drawLine(centerX + innerRadius, 0f, centerX + radius, 0f, paint)
+        canvas.drawCircle(centerX, centerY, innerRadius, paint)
+        canvas.drawCircle(centerX, centerY, radius, paint)
+        canvas.drawLine(centerX - radius, centerY, centerX - innerRadius, centerY, paint)
+        canvas.drawLine(centerX + innerRadius, centerY, centerX + radius, centerY, paint)
+
+        val activeX = centerX - (silhouetteWidth / 2f)
+        val activeY = centerY + (innerRadius  / 2f)
+        val pad = dpToPx(2f)
+        canvas.drawRoundRect(activeX - pad, activeY - pad,
+                activeX + silhouetteWidth + pad,
+                activeY + silhouetteHeight + pad, cardRadius, cardRadius, cardPunchPaint)
+        drawCardSilhouette(canvas, activeX, activeY)
+
+        val opponentActiveY = centerY - (innerRadius / 2f) - silhouetteHeight
+        canvas.drawRoundRect(activeX - pad, opponentActiveY - pad,
+                activeX + silhouetteWidth + pad,
+                opponentActiveY + silhouetteHeight + pad, cardRadius, cardRadius, cardPunchPaint)
+        drawCardSilhouette(canvas, activeX, opponentActiveY)
     }
 }
