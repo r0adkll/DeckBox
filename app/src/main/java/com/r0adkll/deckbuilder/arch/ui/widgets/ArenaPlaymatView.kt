045b3e166a709e5a67d14e38f5c1a2ecf520a695
Drew Heavner <veedubusc@gmail.com>
2018-09-04 13:02:27 -0400

    Refactored arena custom view rendering

--- app/src/main/java/com/r0adkll/deckbuilder/arch/ui/widgets/ArenaPlaymatView.kt
+++ app/src/main/java/com/r0adkll/deckbuilder/arch/ui/widgets/ArenaPlaymatView.kt
@@ -8,6 +8,7 @@ import com.ftinc.kit.kotlin.extensions.color
 import com.ftinc.kit.kotlin.extensions.dpToPx
 import com.ftinc.kit.kotlin.extensions.spToPx
 import com.r0adkll.deckbuilder.R
+import com.r0adkll.deckbuilder.arch.domain.features.playtest.Board.Player
 
 /**
  * TODO: NAME TO CHANGE
@@ -17,17 +18,23 @@ class ArenaPlaymatView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val silhouettePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val silhouetteTextPaint: Paint = Paint(Paint.LINEAR_TEXT_FLAG)
+    private val elementPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val elementTextPaint: Paint = Paint(Paint.LINEAR_TEXT_FLAG)
     private val cardPunchPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    private val pikaCoinImage: Bitmap
-
-    private val benchMargin: Float = dpToPx(24f)
-    private val silhouetteMarginInside: Float = dpToPx(16f)
+    private val boardPadding: Float = dpToPx(24f)
+    private val elementMargin: Float = dpToPx(16f)
+    private var elementWidth: Float = 0f
+    private var elementHeight: Float = 0f
     private val cardRadius = dpToPx(4f)
-    private var silhouetteWidth: Float = 0f
-    private var silhouetteHeight: Float = 0f
+    private val cardPunchPadding = dpToPx(2f)
+
+    /**
+     * A Map of Maps to contain all the positional element components of a playmat board
+     * @see BoardElement
+     * @see Element
+     */
+    private val board: MutableMap<Player.Type, Map<BoardElement, Element>> = HashMap()
 
 
     init {
@@ -36,28 +43,173 @@ class ArenaPlaymatView @JvmOverloads constructor(
         paint.strokeWidth = dpToPx(8f)
         paint.strokeCap = Paint.Cap.ROUND
 
-        silhouettePaint.color = color(R.color.playmat)
-        silhouettePaint.style = Paint.Style.STROKE
-        silhouettePaint.strokeWidth = dpToPx(4f)
-        silhouettePaint.strokeCap = Paint.Cap.ROUND
+        elementPaint.color = color(R.color.playmat)
+        elementPaint.style = Paint.Style.STROKE
+        elementPaint.strokeWidth = dpToPx(4f)
+        elementPaint.strokeCap = Paint.Cap.ROUND
 
-        silhouetteTextPaint.color = color(R.color.playmat)
-        silhouetteTextPaint.textSize = spToPx(14f)
-        silhouetteTextPaint.typeface = Typeface.DEFAULT_BOLD
-        silhouetteTextPaint.textAlign = Paint.Align.CENTER
+        elementTextPaint.color = color(R.color.playmat)
+        elementTextPaint.textSize = spToPx(14f)
+        elementTextPaint.typeface = Typeface.DEFAULT_BOLD
+        elementTextPaint.textAlign = Paint.Align.CENTER
 
         cardPunchPaint.color = Color.WHITE
         cardPunchPaint.style = Paint.Style.FILL_AND_STROKE
         cardPunchPaint.strokeWidth = dpToPx(4f)
 
-        pikaCoinImage = BitmapFactory.decodeResource(resources, R.drawable.ic_pika_coin)
+        // Pre-Allocate all board elements
+        val stadium = Element.Card()
+        val player = HashMap<BoardElement, Element>()
+        player[BoardElement.BENCH] = Element.Bench((0 until 5).map { Element.Card() }.toMutableList())
+        player[BoardElement.DECK] = Element.Card()
+        player[BoardElement.DISCARD] = Element.Card()
+        player[BoardElement.PRIZES] = Element.Card()
+        player[BoardElement.ACTIVE] = Element.Card()
+        player[BoardElement.LOST_ZONE] = Element.Card()
+        player[BoardElement.STADIUM] = stadium
+
+        val opponent = HashMap<BoardElement, Element>()
+        opponent[BoardElement.BENCH] = Element.Bench((0 until 5).map { Element.Card() }.toMutableList())
+        opponent[BoardElement.DECK] = Element.Card()
+        opponent[BoardElement.DISCARD] = Element.Card()
+        opponent[BoardElement.PRIZES] = Element.Card()
+        opponent[BoardElement.ACTIVE] = Element.Card()
+        opponent[BoardElement.LOST_ZONE] = Element.Card()
+        opponent[BoardElement.STADIUM] = stadium
+
+        board[Player.Type.PLAYER] = player
+        board[Player.Type.OPPONENT] = opponent
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-        silhouetteWidth = (measuredWidth.toFloat() -  ((benchMargin * 2)  +  silhouetteMarginInside * 4)) / 5f
-        silhouetteHeight = silhouetteWidth * PokemonCardView.RATIO
+        elementWidth = (measuredWidth.toFloat() -  ((boardPadding * 2)  +  elementMargin * 4)) / 5f
+        elementHeight = elementWidth * PokemonCardView.RATIO
+
+        // Calculate element positioning for both Player.Type's
+
+        // Player.Type.PLAYER
+        run {
+
+            /*
+             * Player Bench
+             */
+            val baseX = boardPadding
+            val baseY = measuredHeight - (boardPadding + elementHeight)
+            val benchElement = board[Player.Type.PLAYER]!![BoardElement.BENCH] as Element.Bench
+            (0 until 5).forEach {
+                val card = benchElement.cards[it]
+                val offsetX = (it * elementWidth) + (it * elementMargin)
+                card.bounds.set(baseX + offsetX, baseY, baseX + offsetX + elementWidth, baseY + elementHeight)
+            }
+
+            /*
+             * Player Discard & Deck
+             */
+            val deckElement = board[Player.Type.PLAYER]!![BoardElement.DECK] as Element.Card
+            val discardElement = board[Player.Type.PLAYER]!![BoardElement.DISCARD] as Element.Card
+
+            val deckX = baseX + (4 * elementWidth) + (4 * elementMargin)
+            val deckY = baseY - ((elementHeight + elementMargin) * 2) + (elementMargin / 2)
+
+            deckElement.bounds.set(deckX, deckY, deckX + elementWidth, deckY + elementHeight)
+            val discardY = deckY + (elementHeight + elementMargin / 2)
+            discardElement.bounds.set(deckX, discardY, deckX + elementWidth, discardY + elementHeight)
+
+            /*
+             * Player Active
+             */
+            val activeElement = board[Player.Type.PLAYER]!![BoardElement.ACTIVE] as Element.Card
+            val centerX = measuredWidth / 2f
+            val centerY = measuredHeight / 2f
+            val radius = measuredWidth / 5f
+            val innerRadius = radius * .40f
+            val activeX = centerX - (elementWidth / 2f)
+            val activeY = centerY + (innerRadius  / 2f)
+            activeElement.bounds.set(activeX, activeY, activeX + elementWidth, activeY + elementHeight)
+
+            /*
+             * Player Prizes
+             */
+            val prizesElement = board[Player.Type.PLAYER]!![BoardElement.PRIZES] as Element.Card
+            val prizesX = baseX
+            val prizesY = baseY + (((activeY - baseY) / 2f) - elementHeight / 2f)
+            prizesElement.bounds.set(prizesX, prizesY, prizesX + elementWidth, prizesY + elementHeight)
+
+            /*
+             * Player Lost Zone
+             */
+            val lostZoneElement = board[Player.Type.PLAYER]!![BoardElement.LOST_ZONE] as Element.Card
+            val lostZoneX = deckX - (elementWidth + elementMargin)
+            val lostZoneY = discardY
+            lostZoneElement.bounds.set(lostZoneX, lostZoneY, lostZoneX + elementWidth, lostZoneY + elementHeight)
+
+            /*
+             * Agnostic Stadium Position
+             */
+            val stadiumElement = board[Player.Type.PLAYER]!![BoardElement.STADIUM] as Element.Card
+            val stadiumX = activeX - (elementWidth + elementMargin)
+            val stadiumY = centerY - (elementHeight / 2f)
+            stadiumElement.bounds.set(stadiumX, stadiumY, stadiumX + elementWidth, stadiumY + elementHeight)
+        }
+
+        // Player.Type.OPPONENT
+        run {
+            /*
+             * Opponent Bench
+             */
+            val benchElement = board[Player.Type.OPPONENT]!![BoardElement.BENCH] as Element.Bench
+            (1 until 6).forEach {
+                val card = benchElement.cards[it - 1]
+                val offsetX = measuredWidth - ((it * elementWidth) + ((it - 1) * elementMargin))
+                val x = offsetX - boardPadding
+                val y = boardPadding
+                card.bounds.set(x, y, x + elementWidth, y + elementHeight)
+            }
+
+            /*
+             * Opponent Discard & Deck
+             */
+            val deckElement = board[Player.Type.OPPONENT]!![BoardElement.DECK] as Element.Card
+            val discardElement = board[Player.Type.OPPONENT]!![BoardElement.DISCARD] as Element.Card
+
+            val deckX = boardPadding
+            val deckY = boardPadding + elementHeight + elementMargin + elementHeight + (elementMargin / 2f)
+
+            deckElement.bounds.set(deckX, deckY, deckX + elementWidth, deckY + elementHeight)
+            val discardY = deckY - (elementHeight + elementMargin / 2f)
+            discardElement.bounds.set(deckX, discardY, deckX + elementWidth, discardY + elementHeight)
+
+            /*
+             * Opponent Active
+             */
+            val activeElement = board[Player.Type.OPPONENT]!![BoardElement.ACTIVE] as Element.Card
+            val centerX = measuredWidth / 2f
+            val centerY = measuredHeight / 2f
+            val radius = measuredWidth / 5f
+            val innerRadius = radius * .40f
+            val activeX = centerX - (elementWidth / 2f)
+            val activeY = centerY - (innerRadius / 2f) - elementHeight
+            activeElement.bounds.set(activeX, activeY, activeX + elementWidth, activeY + elementHeight)
+
+            /*
+             * Opponent Prizes
+             */
+            val prizesElement = board[Player.Type.OPPONENT]!![BoardElement.PRIZES] as Element.Card
+            val baseY = boardPadding + elementHeight + elementMargin
+            val prizesX = measuredWidth - (boardPadding + elementWidth)
+            val prizesY = baseY + ((((activeY + elementHeight) - baseY) / 2f) - (elementHeight / 2f))
+            prizesElement.bounds.set(prizesX, prizesY, prizesX + elementWidth, prizesY + elementHeight)
+
+            /*
+             * Opponent Lost Zone
+             */
+            val lostZoneElement = board[Player.Type.OPPONENT]!![BoardElement.LOST_ZONE] as Element.Card
+            val lostZoneX = deckX + elementWidth + elementWidth + elementMargin
+            val lostZoneY = discardY
+            lostZoneElement.bounds.set(lostZoneX, lostZoneY, lostZoneX + elementWidth, lostZoneY + elementHeight)
+        }
     }
 
 
@@ -67,40 +219,47 @@ class ArenaPlaymatView @JvmOverloads constructor(
          */
         drawPokeBall(canvas)
 
-        /*
-         * Draw the Benches
-         */
-        val baseX = benchMargin
-        val baseY = measuredHeight - (benchMargin + silhouetteHeight)
-        (0 until 5).forEach {
-            val offsetX = (it * silhouetteWidth) + (it * silhouetteMarginInside)
-            drawCardSilhouette(canvas, baseX + offsetX, baseY)
+        // Draw Elements
+        BoardElement.values().forEach { element ->
+            if (element == BoardElement.BENCH) {
+                val player = board[Player.Type.PLAYER]!![element] as Element.Bench
+                val opponent = board[Player.Type.OPPONENT]!![element] as Element.Bench
+
+                player.cards.forEach { drawCard(canvas, it.bounds, null, false) }
+                opponent.cards.forEach { drawCard(canvas, it.bounds, null, false) }
+            } else {
+                drawCard(canvas, element)
+            }
         }
-
-        (1 until 6).forEach {
-            val offsetX = measuredWidth - ((it * silhouetteWidth) + ((it - 1) * silhouetteMarginInside))
-            drawCardSilhouette(canvas, offsetX - benchMargin, benchMargin)
-        }
-
-        /*
-         * Draw the Deck & Discard
-         */
-        val deckX = baseX + (4 * silhouetteWidth) + (4 * silhouetteMarginInside)
-        val deckY = baseY - ((silhouetteHeight + silhouetteMarginInside) * 2) + (silhouetteMarginInside / 2)
-        drawCardSilhouette(canvas, deckX, deckY, "DECK")
-        drawCardSilhouette(canvas, deckX, deckY + (silhouetteHeight + silhouetteMarginInside/2), "DISC.")
-
-        val opponentDeckX = benchMargin
-        val opponentDeckY = benchMargin + silhouetteHeight + silhouetteMarginInside + silhouetteHeight + (silhouetteMarginInside / 2f)
-        drawCardSilhouette(canvas, opponentDeckX, opponentDeckY, "DECK")
-        drawCardSilhouette(canvas, opponentDeckX, opponentDeckY - (silhouetteHeight + silhouetteMarginInside/2f), "DISC.")
     }
 
 
-    private fun drawCardSilhouette(canvas: Canvas, x: Float, y: Float, text: String? = null) {
-        canvas.drawRoundRect(x, y, x + silhouetteWidth, y + silhouetteHeight, cardRadius, cardRadius, silhouettePaint)
+    private fun drawCard(canvas: Canvas, element: BoardElement) {
+        val border = element == BoardElement.ACTIVE || element == BoardElement.STADIUM
+        val text = when(element) {
+            BoardElement.DECK -> "DECK"
+            BoardElement.DISCARD -> "DISC."
+            BoardElement.LOST_ZONE -> "LOST\nZONE"
+            BoardElement.PRIZES -> "PRIZES"
+            else -> null
+        }
+
+        val player = board[Player.Type.PLAYER]!![element]
+        val opponent = board[Player.Type.OPPONENT]!![element]
+        player?.bounds?.let { drawCard(canvas, it, text, border) }
+        opponent?.bounds?.let { drawCard(canvas, it, text, border) }
+    }
+
+
+    private fun drawCard(canvas: Canvas, bounds: RectF, text: String? = null, border: Boolean = false) {
+        if (border) {
+            canvas.drawRoundRect(bounds.left - cardPunchPadding, bounds.top - cardPunchPadding,
+                    bounds.right + cardPunchPadding, bounds.bottom + cardPunchPadding,
+                    cardRadius, cardRadius, cardPunchPaint)
+        }
+        canvas.drawRoundRect(bounds, cardRadius, cardRadius, elementPaint)
         if (text != null) {
-            canvas.drawText(text, x + silhouetteWidth / 2f, (y + silhouetteHeight / 2f), silhouetteTextPaint)
+            canvas.drawText(text, bounds.left + elementWidth / 2f, (bounds.top + elementHeight / 2f), elementTextPaint)
         }
     }
 
@@ -115,19 +274,39 @@ class ArenaPlaymatView @JvmOverloads constructor(
         canvas.drawCircle(centerX, centerY, radius, paint)
         canvas.drawLine(centerX - radius, centerY, centerX - innerRadius, centerY, paint)
         canvas.drawLine(centerX + innerRadius, centerY, centerX + radius, centerY, paint)
+    }
 
-        val activeX = centerX - (silhouetteWidth / 2f)
-        val activeY = centerY + (innerRadius  / 2f)
-        val pad = dpToPx(2f)
-        canvas.drawRoundRect(activeX - pad, activeY - pad,
-                activeX + silhouetteWidth + pad,
-                activeY + silhouetteHeight + pad, cardRadius, cardRadius, cardPunchPaint)
-        drawCardSilhouette(canvas, activeX, activeY)
 
-        val opponentActiveY = centerY - (innerRadius / 2f) - silhouetteHeight
-        canvas.drawRoundRect(activeX - pad, opponentActiveY - pad,
-                activeX + silhouetteWidth + pad,
-                opponentActiveY + silhouetteHeight + pad, cardRadius, cardRadius, cardPunchPaint)
-        drawCardSilhouette(canvas, activeX, opponentActiveY)
+    /**
+     * Per-Player board elements to be represented as [Element]s
+     */
+    enum class BoardElement {
+        BENCH,
+        DECK,
+        DISCARD,
+        ACTIVE,
+        PRIZES,
+        LOST_ZONE,
+        STADIUM
+    }
+
+
+    sealed class Element {
+
+        abstract val bounds: RectF
+
+        class Card(override var bounds: RectF = RectF()) : Element()
+
+        class Bench(val cards: MutableList<Card> = ArrayList() /* 5 - 8 Single Elements */) : Element() {
+
+            override val bounds: RectF
+                get() {
+                    val left = cards.minBy { it.bounds.left }?.bounds?.left ?: 0f
+                    val right = cards.maxBy { it.bounds.right }?.bounds?.right ?: 0f
+                    val top = cards.minBy { it.bounds.top }?.bounds?.top ?: 0f
+                    val bottom = cards.maxBy { it.bounds.bottom }?.bounds?.bottom ?: 0f
+                    return RectF(left, top, right, bottom)
+                }
+        }
     }
 }
