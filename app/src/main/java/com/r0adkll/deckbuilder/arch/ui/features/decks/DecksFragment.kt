4ac6522fd22f0e5c9f2e09ffc4caff7dd4842cdf
Drew Heavner <veedubusc@gmail.com>
2018-09-11 10:11:49 -0400

    Reconfigured the decks layout manager to use the grid layout on phone devices so that the Preview pane is not smashed into one column

--- app/src/main/java/com/r0adkll/deckbuilder/arch/ui/features/decks/DecksFragment.kt
+++ app/src/main/java/com/r0adkll/deckbuilder/arch/ui/features/decks/DecksFragment.kt
@@ -3,6 +3,7 @@ package com.r0adkll.deckbuilder.arch.ui.features.decks
 
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -90,18 +91,23 @@ class DecksFragment : BaseFragment(), DecksUi, DecksUi.Intentions, DecksUi.Actio
 
         adapter.setEmptyView(empty_view)
 
-//        val layoutManager = GridLayoutManager(activity, if (smallestWidth(ScreenUtils.Config.TABLET_10)) 6 else 2)
-//        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-//            override fun getSpanSize(position: Int): Int {
-//                val item = adapter.items[position]
-//                return when(item) {
-//                    Item.Preview -> 2
-//                    else -> 1
-//                }
-//            }
-//        }
 
-        val layoutManager = StaggeredGridLayoutManager(if (smallestWidth(ScreenUtils.Config.TABLET_10)) 6 else 2, StaggeredGridLayoutManager.VERTICAL)
+
+        val layoutManager = if (smallestWidth(ScreenUtils.Config.TABLET_10)) {
+            StaggeredGridLayoutManager(6, StaggeredGridLayoutManager.VERTICAL) as RecyclerView.LayoutManager
+        } else {
+            val lm = GridLayoutManager(activity, 2)
+            lm.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    val item = adapter.items[position]
+                    return when(item) {
+                        Item.Preview -> 2
+                        else -> 1
+                    }
+                }
+            }
+            lm
+        }
 
         recycler.layoutManager = layoutManager
         recycler.adapter = adapter
