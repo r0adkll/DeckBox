a248e14831b8ecb140212736ebe48f790a3b491d
Drew Heavner <veedubusc@gmail.com>
2018-10-29 21:38:16 -0400

    Added a more descriptive hint text for importing cards Added automatic clipboard detection.

--- app/src/main/java/com/r0adkll/deckbuilder/util/extensions/ContextExtensions.kt
+++ app/src/main/java/com/r0adkll/deckbuilder/util/extensions/ContextExtensions.kt
@@ -7,3 +7,7 @@ import android.content.Context
 fun <T> systemService(context: Context, name: String): Lazy<T> = lazy {
     context.getSystemService(name) as T
 }
+@Suppress("UNCHECKED_CAST")
+fun <T> Context.service(name: String): Lazy<T> = lazy {
+    this.getSystemService(name) as T
+}
