b6d2e80a9ec9c2eeed937c50cf9c37bc4e65f369
Drew Heavner <veedubusc@gmail.com>
2018-11-14 11:15:03 -0500

    Finished CacheInvalidatePlugin test Updated dependencies

--- app/src/test/java/com/r0adkll/deckbuilder/arch/data/remote/plugin/CacheInvalidatePluginTest.kt
+++ app/src/test/java/com/r0adkll/deckbuilder/arch/data/remote/plugin/CacheInvalidatePluginTest.kt
@@ -1,8 +1,14 @@
 package com.r0adkll.deckbuilder.arch.data.remote.plugin
 
+import com.nhaarman.mockito_kotlin.verify
 import com.r0adkll.deckbuilder.arch.data.AppPreferences
 import com.r0adkll.deckbuilder.arch.data.features.expansions.ExpansionDataSource
-import org.amshove.kluent.mock
+import com.r0adkll.deckbuilder.arch.domain.features.remote.Remote
+import com.r0adkll.deckbuilder.arch.domain.features.remote.model.ExpansionVersion
+import com.r0adkll.deckbuilder.tools.ModelUtils.EXPANSIONS
+import com.r0adkll.deckbuilder.util.Schedulers
+import io.reactivex.Observable
+import org.amshove.kluent.*
 import org.junit.Before
 
 import org.junit.Assert.*
@@ -12,22 +18,44 @@ class CacheInvalidatePluginTest {
 
     lateinit var preferences: AppPreferences
     lateinit var expansionDataSource: ExpansionDataSource
+    lateinit var remote: Remote
+    lateinit var schedulers: Schedulers
 
     @Before
     fun setUp() {
         preferences = mock()
         expansionDataSource = mock()
+        remote = mock()
+        schedulers = Schedulers.createTrampoline(false)
     }
 
 
     @Test
     fun testInvalidateByVersionCode() {
+        When calling remote.expansionVersion itReturns ExpansionVersion(2, "sm8")
+        When calling expansionDataSource.getExpansions(any()) itReturns Observable.just(EXPANSIONS.toList())
+        When calling expansionDataSource.refreshExpansions() itReturns Observable.just(EXPANSIONS.toList())
+        When calling preferences.expansionsVersion itReturns 1
+        val plugin = CacheInvalidatePlugin(expansionDataSource, preferences, schedulers)
 
+        plugin.onFetchActivated(remote)
+
+        Verify on expansionDataSource that expansionDataSource.refreshExpansions() was called
+        verify(preferences).expansionsVersion = 2
     }
 
 
     @Test
     fun testInvalidateByMissingExpansion() {
+        When calling remote.expansionVersion itReturns ExpansionVersion(1, "sm8")
+        When calling expansionDataSource.getExpansions(any()) itReturns Observable.just(EXPANSIONS.filter { it.code != "sm8" })
+        When calling expansionDataSource.refreshExpansions() itReturns Observable.just(EXPANSIONS.toList())
+        When calling preferences.expansionsVersion itReturns 1
+        val plugin = CacheInvalidatePlugin(expansionDataSource, preferences, schedulers)
 
+        plugin.onFetchActivated(remote)
+
+        Verify on expansionDataSource that expansionDataSource.refreshExpansions() was called
+        verify(preferences).expansionsVersion = 1
     }
 }
