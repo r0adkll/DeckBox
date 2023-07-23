5f9b9f275e62602f6e9b4423dfce1e1cb787e214
Drew Heavner <veedubusc@gmail.com>
2023-07-23 17:31:27 -0400

    Formatting/spotless changes

--- gradle/build-logic/settings.gradle.kts
+++ gradle/build-logic/settings.gradle.kts
@@ -1,14 +1,14 @@
 dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositories {
+    google()
+    mavenCentral()
+  }
 
-    versionCatalogs {
-        create("libs") {
-            from(files("../libs.versions.toml"))
-        }
+  versionCatalogs {
+    create("libs") {
+      from(files("../libs.versions.toml"))
     }
+  }
 }
 
 //buildCache {
