f89db5dce983be1c1933fe9c2f50049c7b0b0c81
Drew Heavner <veedubusc@gmail.com>
2023-08-20 22:14:11 -0400

    making lint happy

--- features/decks/ui/src/commonMain/kotlin/app.deckbox.ui.decks/builder/DeckBuilder.kt
+++ features/decks/ui/src/commonMain/kotlin/app.deckbox.ui.decks/builder/DeckBuilder.kt
@@ -6,6 +6,7 @@ import app.deckbox.common.screens.DeckBuilderScreen
 import app.deckbox.core.di.MergeActivityScope
 import com.r0adkll.kotlininject.merge.annotations.CircuitInject
 
+@Suppress("UNUSED_PARAMETER")
 @CircuitInject(MergeActivityScope::class, DeckBuilderScreen::class)
 @Composable
 fun DeckBuilder(
