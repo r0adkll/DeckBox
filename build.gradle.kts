c1d6c84235a77e8559f45c9dd2570adfb64e0386
Drew Heavner <drew.heavner@reddit.com>
2024-04-27 10:21:39 -0400

    Updating deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,15 @@ plugins {
   alias(libs.plugins.sqldelight) apply false
 }
 
+buildscript {
+  dependencies {
+    // Yuck. Need to force kotlinpoet:1.16.0 as that is what buildconfig uses.
+    // CMP 1.6.0-x uses kotlinpoet:1.14.x. Gradle seems to force 1.14.x which then breaks
+    // buildconfig.
+    classpath("com.squareup:kotlinpoet:1.16.0")
+  }
+}
+
 allprojects {
   val projectName = this.path
   tasks.withType<KotlinCompilationTask<*>>().configureEach {
